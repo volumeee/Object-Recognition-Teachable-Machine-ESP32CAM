d4a33bcd3bad83173aebaea36edcf6c758e4b8b2
volumeee <mr.volumee@gmail.com>
2023-12-01 14:52:46 +0700

    first commit

---
+++ esp32cam_stream.ino
@@ -0,0 +1,153 @@
+#include "src/OV2640.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+#include <WiFiManager.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+OV2640 cam;
+
+WebServer server(80);
+
+// Kredensial WiFi default
+#define DEFAULT_SSID "wifi"
+#define DEFAULT_PWD "pass"
+
+const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
+                      "Access-Control-Allow-Origin: *\r\n" \
+                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
+const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
+const int hdrLen = strlen(HEADER);
+const int bdrLen = strlen(BOUNDARY);
+const int cntLen = strlen(CTNTTYPE);
+
+const char JHEADER[] = "HTTP/1.1 200 OK\r\n" \
+                       "Content-disposition: inline; filename=capture.jpg\r\n" \
+                       "Content-type: image/jpeg\r\n\r\n";
+const int jhdLen = strlen(JHEADER);
+
+void configModeCallback(WiFiManager *myWiFiManager)
+{
+  Serial.println("Entered config mode. Connect to AP: " + String(myWiFiManager->getConfigPortalSSID()));
+}
+
+void handle_jpg_stream(void)
+{
+  char buf[32];
+  int s;
+
+  WiFiClient client = server.client();
+
+  client.write(HEADER, hdrLen);
+  client.write(BOUNDARY, bdrLen);
+
+  while (true)
+  {
+    if (!client.connected()) break;
+    cam.run();
+    s = cam.getSize();
+
+    // Send the HTML and JavaScript content
+    client.write(CTNTTYPE, cntLen);
+    sprintf(buf, "%d\r\n\r\n", s);
+    client.write(buf, strlen(buf));
+    client.write((char *)cam.getfb(), s);
+    client.write(BOUNDARY, bdrLen);
+  }
+}
+
+void handle_jpg(void)
+{
+  WiFiClient client = server.client();
+
+  cam.run();
+  if (!client.connected()) return;
+
+  client.write(JHEADER, jhdLen);
+  client.write((char *)cam.getfb(), cam.getSize());
+}
+
+void handleNotFound()
+{
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text / plain", message);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // Inisialisasi WiFiManager
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModeCallback);
+
+  // Coba menghubungkan ke WiFi dengan kredensial default
+  if (!wifiManager.autoConnect(DEFAULT_SSID, DEFAULT_PWD))
+  {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    // Reset dan mencoba kembali atau lakukan tindakan lain jika diperlukan
+    ESP.restart();
+    delay(5000);
+  }
+
+  // Jika berhasil terhubung, tampilkan informasi IP
+  Serial.println("Connected to WiFi");
+  Serial.println("IP Address: " + WiFi.localIP().toString());
+
+  // Konfigurasi kamera dan pengaturan server
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  // Frame parameters
+  //  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_CIF;
+  config.jpeg_quality = 12;
+  config.fb_count = 2;
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  cam.init(config);
+
+  server.on("/mjpeg/1", HTTP_GET, handle_jpg_stream);
+  server.on("/jpg", HTTP_GET, handle_jpg);
+  server.onNotFound(handleNotFound);
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+}
